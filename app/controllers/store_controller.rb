649ec8ec3c0403a6c17fc8b2964478ec221b67d3
jsmld <samvanerpersroijaards@gmail.com>
2023-07-08 15:34:41 -0300

    Adds session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 1 # Initialize the counter to 1 if it doesn't exist
+    else
+      session[:counter] += 1 # Increment the counter if it exists
+    end
+
+    @counter = session[:counter]
   end
 end
