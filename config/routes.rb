3496eca469191cb93b49ad7bd849f4b8f7306110
jsmld <samvanerpersroijaards@gmail.com>
2023-09-03 23:52:02 -0300

    Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,15 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
